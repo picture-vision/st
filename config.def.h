1917b5824fafdc2a7c483ef964cd9dda28080005
BaTemaN <fw@bateman.de>
2021-04-27 21:16:18 +0200

    changed font and alpha

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:size=13:antialias=true:autohint=true";
+static char *font = "Liberation Mono:size=13:antialias=true:autohint=true";
 static int borderpx = 8;
 
 /*
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.95;
+float alpha = 0.90;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
