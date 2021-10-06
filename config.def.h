2d8aba6327bf6a80359a032c4331c2b0d9c59cca
BaTemaN <fw@picture-vision.com>
2021-10-06 15:06:32 +0200

    updated config

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation mono:size=12:antialias=true:autohint=true";
-static int borderpx = 8;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
