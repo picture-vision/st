7d3df10f4acd55e687f5c163763c4adc6b49ef9e
picture-vision <fw@picture-vision.com>
2022-04-17 11:15:47 +0200

    modified alpha

--- config.def.h
+++ config.def.h
@@ -93,10 +93,10 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.90;
+float alpha = 0.96;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
