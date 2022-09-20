2e25459144797288b64d78a4d53891a15a4b4449
FW <fw@picture-vision.com>
2022-09-20 16:33:07 +0200

    Revert "modified alpha"
    This reverts commit 7d3df10f4acd55e687f5c163763c4adc6b49ef9e.

--- config.def.h
+++ config.def.h
@@ -93,10 +93,10 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 4;
+unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.96;
+float alpha = 0.90;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
