6493bad208b0e654f48396c93ef3c0345c36e969
FW <fw@picture-vision.com>
2024-03-28 22:42:39 +0100

    catppuccin theme

--- config.def.h
+++ config.def.h
@@ -100,36 +100,40 @@ float alpha = 0.92;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-   /* 8 normal colors */
-   [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-   [1] = "#ea6962", /* red     */
-   [2] = "#a9b665", /* green   */
-   [3] = "#d8a657", /* yellow  */
-   [4] = "#7daea3", /* blue    */
-   [5] = "#d3869b", /* magenta */
-   [6] = "#89b482", /* cyan    */
-   [7] = "#d4be98", /* white   */
- 
-   /* 8 bright colors */
-   [8]  = "#928374", /* black   */
-   [9]  = "#ef938e", /* red     */
-   [10] = "#bbc585", /* green   */
-   [11] = "#e1bb7e", /* yellow  */
-   [12] = "#9dc2ba", /* blue    */
-   [13] = "#e1acbb", /* magenta */
-   [14] = "#a7c7a2", /* cyan    */
-   [15] = "#e2d3ba", /* white   */
+	/* 8 normal colors */
+	"#494D64",
+	"#ED8796",
+	"#A6DA95",
+	"#EED49F",
+	"#8AADF4",
+	"#F5BDE6",
+	"#8BD5CA",
+	"#B8C0E0",
+
+	/* 8 bright colors */
+	"#5B6078",
+	"#ED8796",
+	"#A6DA95",
+	"#EED49F",
+	"#8AADF4",
+	"#F5BDE6",
+	"#8BD5CA",
+	"#A5ADCB",
+
+[256] = "#CAD3F5", /* default foreground colour */
+[257] = "#24273A", /* default background colour */
+[258] = "#F4DBD6", /*575268*/
+
 };
 
 
 /*
- * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * Default shape of cursor
